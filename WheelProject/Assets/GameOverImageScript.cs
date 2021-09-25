8e732efa31b0635b2571d9660c894f1b3962954f
Edret0 <jnava096@fiu.edu>
2021-09-25 16:44:54 -0400

    Hope this works lol

--- WheelProject/Assets/GameOverImageScript.cs
+++ WheelProject/Assets/GameOverImageScript.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyScript : MonoBehaviour
+public class GameOverImageScript : MonoBehaviour
 {
-    public float speed = 7;
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transfrom.Translate(Vector3.down * speed * time.DeltaTime);
+        
     }
 }
