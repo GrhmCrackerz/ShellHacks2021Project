43443f5a83445cec17a3cd21ed02904ec5223bbb
FelixRodriguezJr <79116375+FelixRodriguezJr@users.noreply.github.com>
2021-09-25 16:45:33 -0400

    HealthBar

---
+++ WheelProject/Assets/GameHandeler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class GameHandeler : MonoBehaviour
+{
+
+    [SerializeField] private HealthBar healthBar;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        float health = 1f;
+        FunctionPeriodic.Create(() => {
+            if (health > .01f) {
+                health -= .01f;
+                healthBar.SetSize(health);
+            
+                if (health < .3f) { 
+                    // Under 30% Health
+                    if (health * 100f % 3 == 0) {
+                        healthBar.setColor(Color.white);  
+                    }else{
+                        healthBar.setColor(Color.red); 
+                    }
+                }
+            }
+        }, .06f);
+    }
+}
